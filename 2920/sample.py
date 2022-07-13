8e155fc15fe27b4c3f3ad1b1e1848ce0b6da3ec2
DESKTOP-3IVCKQQ\lkas7 <lkas78@naver.com>
2022-07-13 23:50:59 +0900

    [BAEKJOON] 2920번 음계

---
+++ 2920/sample.py
@@ -0,0 +1,17 @@
+inVal = list(map(int, input().split()))
+
+ascType = True
+descType = True
+
+for idx in range(1, len(inVal)):
+    if inVal[idx] > inVal[idx - 1]:
+        descType = False
+    elif inVal[idx] < inVal[idx - 1]:
+        ascType = False
+
+if ascType:
+    print("ascending")
+elif descType:
+    print("descending")
+else:
+    print("mixed")
